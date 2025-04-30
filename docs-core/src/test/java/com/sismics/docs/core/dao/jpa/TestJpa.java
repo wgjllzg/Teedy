20d3dfa3c5bd40e1e62acd26a434332f7e109c1c
wgjllzg <wugujiliu@LAPTOP-OT4GAFT7>
2025-04-30 14:07:35 +0800

    lab10

--- docs-core/src/test/java/com/sismics/docs/core/dao/jpa/TestJpa.java
+++ docs-core/src/test/java/com/sismics/docs/core/dao/jpa/TestJpa.java
@@ -34,4 +34,28 @@ public class TestJpa extends BaseTransactionalTest {
         userDao.delete("testJpa", user.getId());
         TransactionUtil.commit();
     }
+
+    @Test
+    public void testUpdatePassword() throws Exception {
+        // Create a user
+        UserDao userDao = new UserDao();
+        User user = createUser("testUpdatePassword");
+        TransactionUtil.commit();
+    
+        // Update the user's password
+        String newPassword = "newPassword123";
+        user.setPassword(newPassword);
+        User updatedUser = userDao.updatePassword(user, "testUserId");
+        TransactionUtil.commit();
+    
+        // Verify the password was updated
+        User fetchedUser = userDao.getById(updatedUser.getId());
+        Assert.assertNotNull(fetchedUser);
+        Assert.assertNotEquals("12345678", fetchedUser.getPassword()); // Ensure the password is different from the old one
+    
+        // Clean up: Delete the created user
+        userDao.delete("testUpdatePassword", fetchedUser.getId());
+        TransactionUtil.commit();
+    }
+    
 }
